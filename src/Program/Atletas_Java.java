80e34bd5489e2a8253139001e17097505dcf2a0e
Kengi Ortega <kortega90@hotmail.com>
2022-08-18 22:06:54 -0300

    first commit

--- src/Program/Atletas_Java.java
+++ src/Program/Atletas_Java.java
@@ -10,45 +10,79 @@ public class Atletas_Java {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Qual a quantidade de atletas? ");
+		System.out.print("Qual a quantidade de atletas? ");
 		int N = sc.nextInt();
 
-		for (int i = 0; (i < N); i++) {
+		String TallestAthlete = null;
+		double sumWeights = 0;
+		double maxHeight = 0;
+		int numberMen = 0;
+		double sumHeightWomen = 0;
+		int countWomen = 0;
 
+		for (int i = 0; (i < N); i++) {
+			
+			System.out.println("Digite os dados do atleta numero "+ (i+1) +":");
 			System.out.print("Nome: ");
 			String name = sc.next();
 
 			System.out.print("Sexo: ");
+			char gender_ = sc.next().charAt(0);
+			// Char toUpperCase
+			char gender = Character.toUpperCase(gender_);
+			
+			while ((gender != 'M' && gender != 'F')) {
 
-			char gender = sc.next().charAt(0);
-
-			while (!(gender == 'M' && gender == 'F')) {
-
-				System.out.println("invalido! Favor digitar F ou M: ");
+				System.out.print("invalido! Favor digitar F ou M: ");
 				gender = sc.next().charAt(0);
+			}
+
+			if (gender == 'M') {
+				numberMen += 1;
 			}
 
 			System.out.print("Altura: ");
 			double height = sc.nextDouble();
 			while (height <= 0) {
-				System.out.println("Valor invalido! Favor digitar um valor positivo: ");
+				System.out.print("Valor invalido! Favor digitar um valor positivo: ");
 				height = sc.nextDouble();
+			}
+			if (height > maxHeight) {
+				maxHeight = height;
+				TallestAthlete = name;
+			}
+
+			if (gender == 'F') {
+				sumHeightWomen += height;
+				countWomen += 1;
 			}
 
 			System.out.print("Peso: ");
 			double weight = sc.nextDouble();
-
 			while (weight <= 0) {
 
-				System.out.println("Valor invalido! Favor digitar um valor positivo: ");
+				System.out.print("Valor invalido! Favor digitar um valor positivo: ");
 				weight = sc.nextDouble();
 			}
-			
-		System.out.println("RELATÓRIO: ");
-		System.out.printf("Peso médio dos atletas:  %.2f%n",);
+
+			sumWeights += weight;
 
 		}
 
+		double AverageWeightAthletes = sumWeights / N;
+		double percentageMen = (numberMen * 100) / N;
+		System.out.println("RELATÓRIO: ");
+		System.out.printf("Peso médio dos atletas:  %.2f%n", AverageWeightAthletes);
+		System.out.println("Atleta mais alto: " + TallestAthlete);
+		System.out.printf("Porcentagem de homens:  %.1f %%%n", percentageMen);
+		if (sumHeightWomen == 0) {
+			String averageHeightWomen = "Não há mulheres cadastradas";
+			System.out.println(averageHeightWomen);
+		} else {
+			double averageHeightWomen = sumHeightWomen/countWomen;
+			System.out.println("Altura média das mulheres: %.2f%n " + averageHeightWomen);
+		}
+		
 	}
 
 }
